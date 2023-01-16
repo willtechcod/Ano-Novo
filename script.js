3478427855f83a21d236c7992727cbc81d808b17
WILLIAM <tech@MacBook-Pro.local>
2023-01-16 10:04:38 -0400

    reset year

--- script.js
+++ script.js
@@ -8,31 +8,31 @@ const secondsEl = document.getElementById("seconds");
 
 //criando uma representação do ano novo
 
-const newYears = "1 jan 2023";
+const newYears = "1 jan 2024";
 
 // para o cronometro funcionar tem que criar uma função!
 
-function countdown(){
-    
+function countdown() {
+
     const newYearsDate = new Date(newYears);
     const currentDate = new Date();
 
     const totalSeconds = (newYearsDate - currentDate) / 1000;
 
     const days = Math.floor(totalSeconds / 3600 / 24);
-    const hours  = Math.floor(totalSeconds/ 3600) % 24;
+    const hours = Math.floor(totalSeconds / 3600) % 24;
     const mins = Math.floor(totalSeconds / 60) % 60;
     const seconds = Math.floor(totalSeconds) % 60;
 
-//ineerHTML retorna todo testo e htlm que existe 
-    
+    //ineerHTML retorna todo testo e htlm que existe 
+
     daysEl.innerHTML = days;
     hoursEl.innerHTML = formatTime(hours);
     minsEl.innerHTML = formatTime(mins);
     secondsEl.innerHTML = formatTime(seconds);
 }
 
-function formatTime(time){
+function formatTime(time) {
     return time < 10 ? `0${time}` : time;
 }
 
